20c7f132b4cd18d817532324001323874bf14cf9
예균 김 <yk6489@cu.ac.kr>
2019-06-03 14:42:22 +0900

    25

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -2,90 +2,159 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define cQ_SIZE 4
 
 typedef char element;
-typedef struct {
-	element queue[cQ_SIZE];
-	int front, rear;
-}QueueType;
+typedef struct treeNode {
+	char key;
+	struct treeNode* left;
+	struct treeNode* right;
+}treeNode;
 
-QueueType *creatQueue() {
-	QueueType *cQ;
-	cQ = (QueueType *)malloc(sizeof(QueueType));
-	cQ->front = 0;
-	cQ->rear = 0;
-	return cQ;
-}
-
-int isEmpty(QueueType *cQ) {
-	if (cQ->front == cQ->rear) {
-		printf(" Circular Queue is empty! ");
-		return 1;
+treeNode* searchBST(treeNode* root, char x) {
+	treeNode* p;
+	p = root;
+	while (p != NULL) {
+		if (x < p->key)p = p->left;
+		else if (x == p->key)return p;
+		else p = p->right;
 	}
-	else return 0;
+	printf("\n 찾는 키가 없습니다! \n");
+	return p;
 }
 
-int isFull(QueueType *cQ) {
-	if (((cQ->rear + 1) % cQ_SIZE) == cQ->front) {
-		printf(" Circular Queue is full! ");
-		return 1;
+treeNode* insertNode(treeNode *p, char x) {
+	treeNode *newNode;
+	if (p == NULL) {
+		newNode = (treeNode*)malloc(sizeof(treeNode));
+		newNode->key = x;
+		newNode->left = NULL;
+		newNode->right = NULL;
+		return newNode;
 	}
-	else return 0;
+	else if (x < p->key)p->left = insertNode(p->left, x);
+	else if (x > p->key) p->right = insertNode(p->right, x);
+	else printf("\n 이미 같은 키가 있습니다! \n");
+	return p;
 }
 
-void enQueue(QueueType *cQ, element item) {
-	if (isFull(cQ)) return;
+void deleteNode(treeNode *root, element key) {
+	treeNode *parent, *p, *succ, *succ_parent;
+	treeNode *child;
+	parent = NULL;
+	p = root;
+	while ((p != NULL) && (p->key != key)) {
+		parent = p;
+		if (key < p->key)p = p->left;
+		else p = p->right;
+	}
+	if (p == NULL) {
+		printf("\n 찾는 키가 이진 트리에 없습니다!! ");
+		return;
+	}
+
+	if ((p->left == NULL) && (p->right == NULL)) {
+		if (parent != NULL) {
+			if (parent->left == p)parent->left = NULL;
+			else parent->right = NULL;
+		}
+		else root = NULL;
+	}
+	else if ((p->left == NULL) || (p->right == NULL)) {
+		if(p->left !=NULL)child=p->left;
+		else child=p->right;
+
+		if (parent != NULL) {
+			if (parent->left == p)parent->left = child;
+			else parent->right = child;
+		}
+		else root = child;
+	}
 	else {
-		cQ->rear = (cQ->rear + 1) % cQ_SIZE;
-		cQ->queue[cQ->rear] = item;
+		succ_parent = p;
+		succ = p->left;
+		while (succ->right != NULL) {
+			succ_parent = succ;
+			succ = succ->right;
+		}
+		if (succ_parent->left == succ)succ_parent->left = succ->left;
+		else succ_parent->right = succ->left;
+		p->key = succ->key;
+		p = succ;
+	}
+	free(p);
+}
+
+void displayInorder(treeNode* root) {
+	if (root) {
+		displayInorder(root->left);
+		printf("%c_", root->key);
+		displayInorder(root->right);
 	}
 }
 
-element deQueue(QueueType *cQ) {
-	if (isEmpty(cQ))exit(1);
-	else {
-		cQ->front = (cQ->front + 1) % cQ_SIZE;
-		return cQ->queue[cQ->front];
+void menu() {
+	printf("\n*-------------------*");
+	printf("\n\t1 : 트리 출력");
+	printf("\n\t2 : 문자 삽입");
+	printf("\n\t3 : 문자 삭제");
+	printf("\n\t4 : 문자 검색");
+	printf("\n\t5 : 종료");
+	printf("\n*-------------------*");
+	printf("\n메뉴 입력 >> ");
+}
+
+
+
+
+int main() {
+	treeNode* root = NULL;
+	treeNode* foundedNode = NULL;
+	char choice, key;
+
+	root = insertNode(root, 'G');
+	insertNode(root, 'I');
+	insertNode(root, 'H');
+	insertNode(root, 'D');
+	insertNode(root, 'B');
+	insertNode(root, 'M');
+	insertNode(root, 'N');
+	insertNode(root, 'A');
+	insertNode(root, 'J');
+	insertNode(root, 'E');
+	insertNode(root, 'Q');
+	while (1) {
+		menu();
+		scanf(" %c", &choice);
+
+		switch (choice - '0') {
+		case 1:printf("\t[이진 트리 출력]");
+			displayInorder(root); printf("\n");
+			break;
+
+		case 2:printf("삽입할 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+			insertNode(root, key);
+			break;
+
+		case 3:printf("삭제할 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+			deleteNode(root, key);
+			break;
+			
+
+		case 4:printf("찾을 문자를 입력하세요 : ");
+			scanf(" %c", &key);
+            foundedNode = searchBST(root, key);
+			if (foundedNode != NULL)
+				printf("\n %c를 찾았습니다! \n", foundedNode->key);
+			else printf("\n 문자를 찾지 못했습니다. \n");
+			break;
+
+		case 5:return 0;
+
+		default:printf("없는 메뉴입니다. 메뉴를 다시 선택하세요! \n");
+			break;
+		}
 	}
-}
-
-element peek(QueueType *cQ) {
-	if (isEmpty(cQ))exit(1);
-	else return cQ->queue[(cQ->front + 1) % cQ_SIZE];
-}
-
-void printQ(QueueType *cQ) {
-	int i, first, last;
-	first = (cQ->front + 1) % cQ_SIZE;
-	last = (cQ->rear + 1) % cQ_SIZE;
-	printf(" Cisrcular Queue : [");
-	i = first;
-	while (i != last) {
-		printf("%3c", cQ->queue[i]);
-		i = (i + 1) % cQ_SIZE;
-
-	}
-	printf(" ] ");
-}
-
-
-
-void main(void) {
-	QueueType *cQ = creatQueue();
-	element data;
-	printf("\n ***** 원형 큐 연산 ***** \n");
-	printf("\n 삽입 A>>"); enQueue(cQ, 'A'); printQ(cQ);
-	printf("\n 삽입 B>>"); enQueue(cQ, 'B'); printQ(cQ);
-	printf("\n 삽입 C>>"); enQueue(cQ, 'C'); printQ(cQ);
-	data = peek(cQ);       printf(" peek item : %c \n", data);
-	printf("\n 삭제 >>");  data = deQueue(cQ); printQ(cQ);
-	printf("\t삭제 데이터 : %c", data);
-	printf("\n 삭제 >>");  data = deQueue(cQ); printQ(cQ);
-	printf("\t삭제 데이터 : %c", data);
-	printf("\n 삭제 >>");  data = deQueue(cQ); printQ(cQ);
-	printf("\t\t삭제 데이터 : %c", data);
-	printf("\n 삽입 D>>"); enQueue(cQ, 'D'); printQ(cQ);
-	printf("\n 삽입 E>>"); enQueue(cQ, 'E'); printQ(cQ);
-	getchar();
+	
 }
